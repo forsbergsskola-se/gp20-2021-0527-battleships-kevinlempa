6e7355ac6f681bc31d8c22329528df9b69829831
Kevin Lempa <kevin.p.lempa@gmail.com>
2021-05-31 19:36:21 +0200

    adds Fibonacci Recursion

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -13,10 +13,25 @@ void FibonacciIeration(int max) {
 		b = ab;
 		ab = a + b;
 	}
+	cout << endl;
+}
+
+void FibonacciRecursion(int max, int arg1, int arg2) {
+	if (arg1 == 0 && arg2 == 1)
+		cout << "Fibonacci Numbers: " << arg1 << ", " << arg2 << ", ";
+	int a = arg1;
+	int b = arg2;
+	int ab = a + b;
+	cout << ab << ", ";
+	a = b;
+	b = ab;
+	ab = a + b;
+	if (ab >= max)return;
+	FibonacciRecursion(max, a, b);
 }
 
 
-
 int main() {
-	FibonacciIeration(300);
+	FibonacciIeration(300);	
+	FibonacciRecursion(300);
 }
--- Fibonacci.h
+++ Fibonacci.h
@@ -1,1 +1,3 @@
+#pragma once
 void FibonacciIeration(int max);
+void FibonacciRecursion(int max, int arg1 = 0,int arg2 = 1);
